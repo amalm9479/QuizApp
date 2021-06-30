061b167c09b257321fca10f9797314982e120140
amalm9479 <amalm9479@gmail.com>
2021-07-01 02:44:44 +0300

    first
    Signed-off-by: amalm9479 <amalm9479@gmail.com>

---
+++ app/src/main/java/com/amjedalmousawi/quizapp/Question.kt
@@ -0,0 +1,16 @@
+package com.amjedalmousawi.quizapp
+
+data class Question(
+    val id:Int,
+    val question: String,
+    val image: Int,
+    val optionOne: String,
+    val optionTwo: String,
+    val optionThree: String,
+    val optionFour: String,
+    val correctAnswer: Int
+
+
+
+    )
+
